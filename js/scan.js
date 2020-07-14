40ba46721d22fc62d409793899cf234a1b0b7422
linzijun <linzjk@yonyou.com>
2020-07-14 15:46:07 +0800

    初始化摄像头列表

--- js/scan.js
+++ js/scan.js
@@ -2,26 +2,36 @@ import openConsole from '../utils/openConsole.js';
 
 class ScanPage {
   constructor() {
-
+    this.operate = document.querySelector('.camera-operate');
+    this.select = document.querySelector('#camera-picker');
+    this.video = document.querySelector('#camera-view');
   }
 
   testConsole() {
     openConsole();
   }
 
-  initMediaStream() {
-    navigator.mediaDevices.enumerateDevices().then(res => {
-      console.log(res);
+  initDevices() {
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+      const videoDevices = devices.filter(device => device.kind === 'videoinput');
+      console.log(videoDevices);
+      this.select.insertAdjacentHTML('beforeend', videoDevices.reduce((acc, device) => {
+        acc += `<option value="${device.deviceId}">${device.label}</option>`;
+        return acc;
+      }, ''));
+      this.operate.style.display = 'block';
     });
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(mediaStream => {
-        const video = document.querySelector('#camera-view');
-        video.srcObject = mediaStream;
-      });
+  }
+
+  initMediaStream() {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(mediaStream => {
+      this.video.srcObject = mediaStream;
+    });
   }
 
   init() {
     this.testConsole();
+    this.initDevices();
     this.initMediaStream();
   }
 }
